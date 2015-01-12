46b76d3897e656f8fefe859e04beca7a7d8ece70
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2015-01-11 19:32:30 -0500

    data binding working

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -8,9 +8,12 @@ angular.module('todoApp', [])
 	  this.todos = ['Item 1', 'Item 2', 'Item 3'];
 }
 
-mainController.prototype.addTodo = function() {
-	// this part isn't working
+mainController.prototype.addTodo = function(todo) {
   this.todos.push(todo);
+  console.log('todo before empty string: ' + todo);
+  
+  todo = "";
+  console.log('todo after empty string: ' + todo);
   console.log(this.todos);
 };
 
