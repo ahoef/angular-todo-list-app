fb3239d0f5eb07a8c171044a4afaa5afd6191780
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2015-01-12 14:11:11 -0500

    remove functionality working, complete functionality not working

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,21 +1,34 @@
 (function(angular) {
-  'use strict';
+	'use strict';
+	// var completed = false;
 
-angular.module('todoApp', [])
-  .controller('mainController', mainController);
+	angular.module('todoApp', [])
+	.controller('mainController', mainController);
 
-  function mainController() {
-	  this.todos = ['Item 1', 'Item 2', 'Item 3'];
-}
+	function mainController() {
+		this.todos = ['Item 1', 'Item 2', 'Item 3'];
+	}
 
-mainController.prototype.addTodo = function(todo) {
-  this.todos.push(todo);
-  console.log('todo before empty string: ' + todo);
-  
-  todo = "";
-  console.log('todo after empty string: ' + todo);
-  console.log(this.todos);
-};
+	mainController.prototype.addTodo = function(todo) {
+		this.todos.push(todo);
+		console.log('todo before empty string: ' + todo);
+
+		todo = "";
+		console.log('todo after empty string: ' + todo);
+		console.log(this.todos);
+	};
+
+	mainController.prototype.completeTodo = function(todo) {
+		// console.log(completed);
+		var completed = true;
+		console.log(completed);
+	}
+
+	mainController.prototype.removeTodo = function(todo) {
+		var index = this.todos.indexOf(todo);
+		this.todos.splice(index, 1);
+	}
+
 
 })(window.angular);
 
